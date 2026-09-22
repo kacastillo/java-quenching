7b7a0976c46c71143fb165b08e619bbdacb8ec40
kelley <kelleycastillo@outlook.com>
2026-09-22 15:14:56 -0700

    finished shortestword

--- src/Practice.java
+++ src/Practice.java
@@ -22,7 +22,7 @@ public class Practice {
         }
     }
     return sum;
-}
+    }
 
 
     /**
@@ -37,8 +37,22 @@ public class Practice {
      * @throws NullPointerException if words is null
      */
     public static String shortestWord(Set<String> words) {
-        return null;
-    }
+        if (words == null) {
+            throw new NullPointerException("words is null");
+        }
+        if (words.isEmpty()) {
+            throw new IllegalArgumentException("words is empty");
+        }
+        String shortest = null;
+        for (String word : words) {
+        if (shortest == null || word.length() < shortest.length() ||
+         (word.length() == shortest.length() && word.compareTo(shortest) < 0 )) 
+         {
+         shortest = word;
+        }
+        }
+        return shortest;
+        }
 
     /**
      * Returns a set of all the names of people that are 18 years of age or older.
